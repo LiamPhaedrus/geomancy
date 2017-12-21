e7d9eac3c2b7429fa83990d50256738e8fc096a6
LiamPhaedrus <liamphaedrus@gmail.com>
2017-12-21 11:27:32 -0500

    geomanced

---
+++ app/javascript/react/src/containers/HomePage.js
@@ -0,0 +1,17 @@
+import React, { Component }  from 'react';
+import { Link } from 'react-router'
+
+class HomePage extends Component {
+  constructor (props) {
+    super(props)
+  }
+  render () {
+    return (
+      <div>
+      <h1>Greetings mortal</h1>
+      </div>
+    )
+  }
+}
+
+export default HomePage
